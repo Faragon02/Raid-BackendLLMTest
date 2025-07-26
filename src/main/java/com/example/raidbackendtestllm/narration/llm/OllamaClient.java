565dab549a28f0a248249398ed246c6948b6b446
Fragon02 <chj20202@naver.com>
2025-07-26 18:49:02 +0900

    feat: OllamaClient 업데이트

--- src/main/java/com/example/raidbackendtestllm/narration/llm/OllamaClient.java
+++ src/main/java/com/example/raidbackendtestllm/narration/llm/OllamaClient.java
@@ -2,6 +2,8 @@ package com.example.raidbackendtestllm.narration.llm;
 
 import java.util.Map;
 
+import com.example.raidbackendtestllm.narration.dto.OllamaTestResponseDto;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -10,7 +12,7 @@ public class OllamaClient implements LLMClient {
 	private final WebClient webClient = WebClient.create("http://localhost:11434");
 
 	@Override
-	public String generate(String prompt) {
+	public String  generate(String prompt) {
 		Map<String, Object> request = Map.of(
 			"model", "mistral",
 			"prompt", prompt,
@@ -18,11 +20,14 @@ public class OllamaClient implements LLMClient {
 		);
 
 		return webClient.post()
-			.uri("/api/generate")
-			.header("Content-Type", "application/json")
-			.bodyValue(request)
-			.retrieve()
-			.bodyToMono(String.class)
-			.block();
+				.uri("/api/generate")
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(request)
+				.retrieve()
+				.bodyToMono(OllamaTestResponseDto.class)
+				.map(OllamaTestResponseDto::response)
+				.block();
+
 	}
+
 }
