3b253a4256a6ff6305cb56ea4c2adc169440637b
Fragon02 <chj20202@naver.com>
2025-07-26 18:47:50 +0900

    feat: OllamaTest용 DTO 추가

---
+++ src/main/java/com/example/raidbackendtestllm/narration/dto/OllamaTestRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.raidbackendtestllm.narration.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class OllamaTestRequestDto {
+    String prompt;
+}
