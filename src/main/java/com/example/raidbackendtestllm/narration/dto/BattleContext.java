83877eada787de2de320da4ad01595360cf09ff6
Fragon02 <chj20202@naver.com>
2025-07-26 18:50:03 +0900

    feat: 배틀 정보DTO 수정

--- src/main/java/com/example/raidbackendtestllm/narration/dto/BattleContext.java
+++ src/main/java/com/example/raidbackendtestllm/narration/dto/BattleContext.java
@@ -7,17 +7,17 @@ import lombok.Getter;
 @Getter
 public class BattleContext {
 	private String bossName;
-	private String bossHpDescription;
+	private String bossHp;
 	private List<BattleCharacterStatus> party;
-	private List<String> previousUserLines;
-	private List<String> previousBossResponses;
+	private List<String> recentLogs;
+	private String userLine;
 
-	public BattleContext(String bossName, String bossHpDescription, List<BattleCharacterStatus> party,
-		List<String> previousUserLines, List<String> previousBossResponses) {
+	public BattleContext(String bossName, String bossHp, List<BattleCharacterStatus> party,
+		List<String> recentLogs, String userLine) {
 		this.bossName = bossName;
-		this.bossHpDescription = bossHpDescription;
+		this.bossHp = bossHp;
 		this.party = party;
-		this.previousUserLines = previousUserLines;
-		this.previousBossResponses = previousBossResponses;
+		this.recentLogs = recentLogs;
+		this.userLine = userLine;
 	}
 }
